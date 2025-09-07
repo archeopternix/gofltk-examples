798d9865890946c54115c7c32a24febda56e2a40
archeopternix <andreas.eisner@kouri.cc>
2025-09-07 17:17:20 +0200

    refactored into structs

--- sudoku/main.go
+++ sudoku/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"strconv"
 
 	"github.com/archeopternix/go-fltk"
 )
@@ -15,81 +15,121 @@ const (
 )
 
 var midBlue = fltk.ColorFromRgb(66, 135, 245)
-var lightGrey = fltk.ColorFromRgb(230, 230, 230)
-var midGrey = fltk.BACKGROUND_COLOR
+
+type Cell struct {
+	Value  string
+	Widget *fltk.Button
+	i      int
+	j      int
+	active bool
+}
+
+func NewCell(g *SudokuGrid, i, j int) *Cell {
+	extraX := (j / subGrid) * subPadding
+	extraY := (i / subGrid) * subPadding
+	cellX := j*cellSize + extraX + padding
+	cellY := i*cellSize + extraY + padding
+
+	cell := &Cell{
+		Value:  "",
+		Widget: fltk.NewButton(cellX, cellY, cellSize-2, cellSize-2, ""),
+		i:      i,
+		j:      j,
+		active: false,
+	}
+
+	cell.Widget.SetAlign(fltk.ALIGN_CENTER)
+	cell.Widget.SetBox(fltk.BORDER_BOX)
+	if ((i/subGrid)+(j/subGrid))%2 == 1 {
+		cell.Widget.SetColor(fltk.ColorFromRgb(230, 230, 230)) // light gray
+	}
+	// Make selectable on click
+	i_copy := i
+	j_copy := j
+	cell.Widget.SetCallback(func() {
+		g.SelectCell(i_copy, j_copy, true)
+		g.Redraw()
+	})
+
+	return cell
+}
 
 type SudokuGrid struct {
 	*fltk.Group
-	cells        [gridSize][gridSize]*fltk.Button
-	selectedCell *fltk.Button
+	cells         [gridSize][gridSize]*Cell
+	activeCellRow int
+	activeCellCol int
 }
 
 func NewSudokuGrid(x, y, width, height int) *SudokuGrid {
 	grid := &SudokuGrid{
-		Group: fltk.NewGroup(x, y, width, height),
+		Group:         fltk.NewGroup(x, y, width, height),
+		activeCellRow: -1, // Initialize with invalid values
+		activeCellCol: -1,
 	}
 	grid.Begin()
 	for i := 0; i < gridSize; i++ {
 		for j := 0; j < gridSize; j++ {
-			extraX := (j / subGrid) * subPadding
-			extraY := (i / subGrid) * subPadding
-			cellX := j*cellSize + extraX
-			cellY := i*cellSize + extraY
-
-			cell := fltk.NewButton(cellX+x, cellY+y, cellSize-2, cellSize-2, "")
-			cell.SetAlign(fltk.ALIGN_CENTER)
-			cell.SetBox(fltk.BORDER_BOX)
-			if ((i/subGrid)+(j/subGrid))%2 == 1 {
-				cell.SetColor(lightGrey) // light gray
-			} else {
-				cell.SetColor(midGrey) // light gray
-			}
-			// Make selectable on click
-			i_copy := i
-			j_copy := j
-			cell.SetCallback(func() {
-				grid.selectCell(i_copy, j_copy)
-				grid.Redraw()
-			})
-			grid.cells[i][j] = cell
+			grid.cells[i][j] = NewCell(grid, i, j)
 		}
 	}
 	grid.End()
 	return grid
 }
 
-func (g *SudokuGrid) selectCell(i, j int) {
-	fmt.Println(i, ":", j)
-	// Remove frame from previous selected cell
-	if g.selectedCell != nil {
-		g.selectedCell.SetBox(fltk.BORDER_BOX)
-		if ((i/subGrid)+(j/subGrid))%2 == 1 {
-			g.selectedCell.SetColor(midGrey)
-		} else {
-			g.selectedCell.SetColor(lightGrey) // light gray
+func (g *SudokuGrid) SelectCell(i, j int, active bool) {
+	// Deselect all cells
+	if active {
+		for row := 0; row < gridSize; row++ {
+			for col := 0; col < gridSize; col++ {
+				if row != i || col != j {
+					g.cells[row][col].active = false
+					if ((g.cells[row][col].i/subGrid)+(g.cells[row][col].j/subGrid))%2 == 1 {
+						g.cells[row][col].Widget.SetColor(fltk.ColorFromRgb(230, 230, 230)) // light gray
+					} else {
+						g.cells[row][col].Widget.SetColor(fltk.BACKGROUND_COLOR)
+					}
+					g.cells[row][col].Widget.SetBox(fltk.BORDER_BOX)
+				}
+			}
 		}
 	}
 
-	g.selectedCell = g.cells[i][j]
-	// Set blue color for the new selected cell
-	g.selectedCell.SetColor(midBlue)
-	g.selectedCell.SetBox(fltk.THIN_UP_BOX)
-	g.Redraw() // Redraw the SudokuGrid after selecting a cell
+	// Select the current cell
+	g.cells[i][j].active = active
+	if active {
+		g.cells[i][j].Widget.SetColor(midBlue)
+		g.cells[i][j].Widget.SetBox(fltk.THIN_UP_BOX)
+		g.activeCellRow = i
+		g.activeCellCol = j
+	} else {
+		if ((g.cells[i][j].i/subGrid)+(g.cells[i][j].j/subGrid))%2 == 1 {
+			g.cells[i][j].Widget.SetColor(fltk.ColorFromRgb(230, 230, 230)) // light gray
+		} else {
+			g.cells[i][j].Widget.SetColor(fltk.BACKGROUND_COLOR)
+		}
+		g.cells[i][j].Widget.SetBox(fltk.BORDER_BOX)
+		g.activeCellRow = -1
+		g.activeCellCol = -1
+	}
+	g.Redraw()
 }
 
-func (g *SudokuGrid) onCellSelected(cell *fltk.Button) {
-	// Custom callback for when a cell is selected
-	// You can replace this with your logic
-	println("Cell selected!")
-}
-
-func (g *SudokuGrid) GetSelectedCell() *fltk.Button {
-	return g.selectedCell
+func (g *SudokuGrid) GetCell(i, j int) string {
+	return g.cells[i][j].Value
 }
 
 func (g *SudokuGrid) SetCell(i, j int, text string) {
-	g.cells[i][j].SetLabel(text)
-	g.Redraw()
+	// test if text is a number
+	val, err := strconv.Atoi(text)
+	if err != nil {
+		text = ""
+	}
+	if (val > 0) && (val <= 9) {
+		g.cells[i][j].Value = text
+		g.cells[i][j].Widget.SetLabel(text)
+		g.Redraw()
+	}
 }
 
 func main() {
