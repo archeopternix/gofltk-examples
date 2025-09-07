c295a3b8d5ee797e5ff3d4564588572d6a55515a
archeopternix <andreas.eisner@kouri.cc>
2025-09-07 16:13:33 +0200

    improvements

--- sudoku/main.go
+++ sudoku/main.go
@@ -1,37 +1,73 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/archeopternix/go-fltk"
 )
 
 const (
-	gridSize = 9
-	cellSize = 40
-	padding  = 10
+	gridSize   = 9
+	subGrid    = 3
+	cellSize   = 40
+	padding    = 10
+	subPadding = 4 // extra space between subgrids
 )
 
-func main() {
-	win := fltk.NewWindow(gridSize*cellSize+2*padding, gridSize*cellSize+2*padding)
-	win.SetLabel("Sudoku Board - go-fltk")
+var selectedCell *fltk.Output // Global variable to store the selected cell
 
-	// 2D array of input boxes
-	var cells [gridSize][gridSize]*fltk.Input
+func selectCell(i, j int) {
+	fmt.Println(i, ":", j)
+	// Remove frame from previous selected cell
+	if selectedCell != nil {
+		selectedCell.SetColor(fltk.BACKGROUND_COLOR)
+		selectedCell.SetBox(fltk.BORDER_BOX)
+	}
+
+	selectedCell = cells[i][j]
+	// Set blue frame for the new selected cell
+	selectedCell.SetColor(fltk.BACKGROUND_COLOR)
+	selectedCell.SetBox(fltk.THIN_UP_BOX)
+	selectedCell.SetSelectionColor(fltk.ColorFromRgb(0, 120, 255)) // blue frame
+
+	// Trigger your callback here
+	onCellSelected(selectedCell)
+}
+
+func onCellSelected(cell *fltk.Output) {
+	// Custom callback for when a cell is selected
+	// You can replace this with your logic
+	println("Cell selected!")
+}
+
+var cells [gridSize][gridSize]*fltk.Output
+
+func main() {
+	winWidth := gridSize*cellSize + 2*padding + (subGrid-1)*subPadding
+	winHeight := gridSize*cellSize + 2*padding + (subGrid-1)*subPadding
+
+	win := fltk.NewWindow(winWidth, winHeight)
+	win.SetLabel("Sudoku Board - go-fltk (Read Only, Selectable)")
 
 	for i := 0; i < gridSize; i++ {
 		for j := 0; j < gridSize; j++ {
-			x := padding + j*cellSize
-			y := padding + i*cellSize
-			input := fltk.NewInput(x, y, cellSize-2, cellSize-2, "")
-			//	input.SetMaximumSize(cellSize-2, cellSize-2)
-			input.SetAlign(fltk.ALIGN_CENTER)
-			//	input.SetTextSize(20)
-			input.SetCallback(func() {
-				val := input.Value()
-				if len(val) > 1 || (len(val) == 1 && (val[0] < '1' || val[0] > '9')) {
-					input.SetValue("")
-				}
+			extraX := (j / subGrid) * subPadding
+			extraY := (i / subGrid) * subPadding
+			x := padding + j*cellSize + extraX
+			y := padding + i*cellSize + extraY
+
+			cell := fltk.NewOutput(x, y, cellSize-2, cellSize-2, "")
+			cell.SetAlign(fltk.ALIGN_CENTER)
+			cell.SetBox(fltk.BORDER_BOX)
+			if ((i/subGrid)+(j/subGrid))%2 == 1 {
+				cell.SetColor(fltk.ColorFromRgb(230, 230, 230)) // light gray
+			}
+			// Make selectable on click
+			cell.SetCallback(func() {
+				selectCell(i, j)
+				win.Redraw()
 			})
-			cells[i][j] = input
+			cells[i][j] = cell
 		}
 	}
 
